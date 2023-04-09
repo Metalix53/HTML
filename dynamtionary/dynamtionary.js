a8c55e65347f6e5269deb7fa1d32ecca9d47cbce
Max Mancini <75049386+Metalix53@users.noreply.github.com>
2023-04-09 18:58:59 -0400

    Add files via upload

--- dynamtionary/dynamtionary.js
+++ dynamtionary/dynamtionary.js
@@ -1,6 +1,8 @@
 console.log('did somebody invite dynamtionary??');
 const text = document.getElementById("text");
+const inp = document.getElementById("inp");
 const div = document.getElementById("dynamtionary");
+let allWords = new Map();
 let dict = new Map();
 
 text.addEventListener('keypress', function (e){
@@ -10,23 +12,47 @@ text.addEventListener('keypress', function (e){
       text.value = '';
       arr.forEach(element => {
         if (element != ""){
-          element = element.replace(/(\r\n|\n|\r)/gm, "");
+          element = element.replace(/(\r\n|\n|\r|\s)/gm, "");
           addToDict(element);
+          update(element);
         }
       });
     }
   }
 });
 
+inp.addEventListener('keypress', function (e){
+  if (document.activeElement == inp){
+    if (e.key === 'Enter'){
+      var word = inp.value;
+      inp.value = '';
+      if (!dict.has(word))
+      {
+        dict.set(word, word);
+        update(word);
+      }
+    }
+  }
+});
+
 function addToDict(word) {
-  if (!dict.has(word)){
-    dict.set(word, word);
+  if (!allWords.has(word))
+  {
+    allWords.set(word, word);
     const node = document.createElement("span");
+    node.setAttribute('id', word);
     const brk = document.createElement("br");
     const text = document.createTextNode(word);
     node.appendChild(text);
     node.appendChild(brk);
     div.appendChild(node);
-    console.table(dict);
+  }
+}
+
+function update(word){
+  if (allWords.has(word) && dict.has(word))
+  {
+    const elem = document.getElementById(word);
+    elem.style = "background-color: green; font-weight: bold;"
   }
 }
